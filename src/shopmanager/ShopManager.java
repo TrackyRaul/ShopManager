648efb5542997024027df0b619d7cca606e46549
Raul Farkas <raul.farkas19@gmail.com>
2018-12-24 14:35:41 +0200

    Initial commit with basic java project

---
+++ src/shopmanager/ShopManager.java
@@ -0,0 +1,15 @@
+package shopmanager;
+
+/**
+ *
+ * @author Raul Farkas
+ */
+public class ShopManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
